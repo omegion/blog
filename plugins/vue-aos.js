e32910221031463072805989b79136b6f957ec3e
omegion <kurtulus@mail.com>
2021-04-10 12:01:07 +0200

    Fix AOS animation on reload

--- plugins/vue-aos.js
+++ plugins/vue-aos.js
@@ -5,7 +5,8 @@ import "aos/dist/aos.css";
 export default ({ app }) => {
   // eslint-disable-next-line new-cap
   app.AOS = new AOS.init({
-    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-    once: true, // whether animation should happen only once - while scrolling down
+    disable: false,
+    startEvent: "load",
+    once: true,
   });
 };
